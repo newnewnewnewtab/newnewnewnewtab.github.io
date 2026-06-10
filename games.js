8c9110025c088827ad75a4c75a995d68d77aef94
Cling.Py <154641389+newnewnewnewtab@users.noreply.github.com>
2026-06-10 12:52:34 -0400

    Refactor game entries and update ribbon colors
    Removed commented potential URLs and updated ribbon properties for several games.

--- games.js
+++ games.js
@@ -1,10 +1,4 @@
 window.PAGES = {
-  //Pottentials 
-  //https://script.google.com/macros/s/AKfycbw8iHPqdVFEzquUYbNxFVAu1Tw4Nri5SWMRLdP_c7a84vCOHVG7YUWuhjSVptg1SVHr/exec
-  //https://script.google.com/macros/s/AKfycbzGxfKS5Uv4MC2N__iKoytC7F5HxVSH3GBPkkqKc0WLf7aIOFkZr3AMsN03VbEPKMGQ/exec
-  //https://script.google.com/macros/s/AKfycbwfUFXy27bYwMfp4xHUVbpN9PSWWzB7bKs-qA01ZYJ7hgeZbvFZgkdjydsAYWrYGPQb/exec
-  
-
   
   "Drive Mad": { 
     "url": "https://newnewnewnewtab.github.io/drive-mad/drive-mad-new/"
@@ -23,9 +17,25 @@ window.PAGES = {
   },
 
    "Paper.io": { 
-    "url": "https://script.google.com/macros/s/AKfycbwNxCzUuQVmLRWTwYK2D4yTqpWg2O-qge8BwCkxhLmqWd1DtXynwJXPEDClPt7ERZr-/exec",
+    "url": "https://script.google.com/macros/s/AKfycbwNxCzUuQVmLRWTwYK2D4yTqpWg2O-qge8BwCkxhLmqWd1DtXynwJXPEDClPt7ERZr-/exec"
+  },
+
+   "Drift Hunters": { 
+    "url": "https://script.google.com/macros/s/AKfycbw8iHPqdVFEzquUYbNxFVAu1Tw4Nri5SWMRLdP_c7a84vCOHVG7YUWuhjSVptg1SVHr/exec",
     "ribbon": "New",
-    "ribbonColor": "#0096FF"
+    "ribbonColor": "#ff77ff"
+  },
+
+    "Skyblock 2D": { 
+    "url": "https://script.google.com/macros/s/AKfycbzGxfKS5Uv4MC2N__iKoytC7F5HxVSH3GBPkkqKc0WLf7aIOFkZr3AMsN03VbEPKMGQ/exec",
+    "ribbon": "New",
+    "ribbonColor": "#ff77ff"
+  },
+
+    "There is No Game": { 
+    "url": "https://script.google.com/a/macros/rsu71.org/s/AKfycbwfUFXy27bYwMfp4xHUVbpN9PSWWzB7bKs-qA01ZYJ7hgeZbvFZgkdjydsAYWrYGPQb/exec",
+    "ribbon": "New",
+    "ribbonColor": "#ff77ff"
   },
   
   "Subway Surfers": { 
@@ -33,15 +43,11 @@ window.PAGES = {
   },
 
   "Slow Roads": { 
-    "url": "https://script.google.com/macros/s/AKfycbzqDA2SnuVZ3DRelxxbUxSV9Z1RJz_gQfDRx06WUpgppWgrdDEErtZ1Lev9O6j2w9ioBQ/exec",
-    "ribbon": "New",
-    "ribbonColor": "#0096FF"
+    "url": "https://script.google.com/macros/s/AKfycbzqDA2SnuVZ3DRelxxbUxSV9Z1RJz_gQfDRx06WUpgppWgrdDEErtZ1Lev9O6j2w9ioBQ/exec"
   },
 
   "Fishing.io": { 
-    "url": "https://script.google.com/macros/s/AKfycbzZJhJIeHFv_-I9PXF5xA-MA115srBLxJu4hxtMP4z0hGHSBcMzBNydUT5UxfmhSNQq/exec",
-    "ribbon": "New",
-    "ribbonColor": "#0096FF"
+    "url": "https://script.google.com/macros/s/AKfycbzZJhJIeHFv_-I9PXF5xA-MA115srBLxJu4hxtMP4z0hGHSBcMzBNydUT5UxfmhSNQq/exec"
   },
   
   "Moto X3M 2": { 
@@ -54,8 +60,8 @@ window.PAGES = {
 
   "Dig Out Of Prison": { 
     "url": "https://script.google.com/macros/s/AKfycby-5VSTOBeep9g9GU9un8CLFOo4TvTSmahbVEGqze-hX-iVmuLUtIyx6WAj2wvOYQeb/exec",
-    "ribbon": "New",
-    "ribbonColor": "#0096FF"
+    "ribbon": "Popular",
+    "ribbonColor": "#EFBF04"
   },
 
   "Minecraft": { 
